72b6501f349cf147fec88fe221755a177a47937f
Sebash12 <juan_herrerasilva@hotmail.com>
2022-12-11 23:23:44 -0500

    Retiramos a Eduard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
-import Menu from './components/Menu'
-import Eduard from './components/Eduard'
+import Menu from './components/Menu/Menu'
 import React from 'react'
-import Sebas from './components/Sebas'
+import Sebas from './components/Sebas/Sebas'
+import Error from './components/Error/Error'
 import { BrowserRouter, Routes, Route } from "react-router-dom";  
 
 
@@ -15,7 +15,7 @@ class App extends React.Component {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Menu />} />
-            <Route path="/Eduard" element={<Eduard/>} />
+            <Route path='*' element={<Error />} />
             <Route path="/Sebas" element={<Sebas/>} />
           </Routes>
         </BrowserRouter>
