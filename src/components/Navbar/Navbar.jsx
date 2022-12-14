a5ff0ca19e0b4463eda08db278ec8746536b66d8
Sebash12 <juan_herrerasilva@hotmail.com>
2022-12-13 20:20:24 -0500

    Se agrega el navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+function Navbar()
+{
+    return(
+        <div>
+            <nav>
+            <ul>
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/Sebas">Sobre nosotros</a></li>
+                <li><a href="*">Contacto</a></li>
+            </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
